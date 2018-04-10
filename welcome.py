4ec07c53f43333ba6856b05dbcad0319df19d257
piotrkania <36471063+piotrkania@users.noreply.github.com>
2018-04-10 14:44:33 +0200

    Create welcome.py

---
+++ welcome.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+print """\
+
+         WW         WW EEEEEEE LL       CCCCC   OOOOO  MM    MM EEEEEEE
+          WW       WW  EE      LL      CC    C OO   OO MMM  MMM EE
+          WW   W   WW  EEEEE   LL      CC      OO   OO MM MM MM EEEEE
+           WW WWW WW   EE      LL      CC    C OO   OO MM    MM EE
+            WW   WW    EEEEEEE LLLLLLL  CCCCC   OOOO0  MM    MM EEEEEEE
+
+                   TTTTTTT  OOOOO   TTTTTTT HH   HH EEEEEEE
+                     TTT   OO   OO    TTT   HH   HH EE
+                     TTT   OO   OO    TTT   HHHHHHH EEEEE
+                     TTT   OO   OO    TTT   HH   HH EE
+                     TTT    OOOO0     TTT   HH   HH EEEEEEE
+
+            DDDDD   UU   UU NN   NN   GGGG  EEEEEEE  OOOOO  NN   NN
+            DD  DD  UU   UU NNN  NN  GG  GG EE      OO   OO NNN  NN
+            DD   DD UU   UU NN N NN GG      EEEEE   OO   OO NN N NN
+            DD   DD UU   UU NN  NNN GG   GG EE      OO   OO NN  NNN
+            DDDDDD   UUUUU  NN   NN  GGGGGG EEEEEEE  OOOO0  NN   NN
+
+                            RRRRRR  UU   UU NN   NN
+                            RR   RR UU   UU NNN  NN
+                            RRRRRR  UU   UU NN N NN
+                            RR  RR  UU   UU NN  NNN
+                            RR   RR  UUUUU  NN   NN
+"""
