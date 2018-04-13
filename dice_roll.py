c5c0c11de2e81ad5455638a920cf6815c94bcc4d
Piotr <pkania@localhost.localdomain>
2018-04-13 21:36:57 +0200

    initial commit

--- dice_roll.py
+++ dice_roll.py
@@ -2,40 +2,34 @@
 
 from random import randrange
 
-print ("*Automatic Dice Roller*")
-print '\n'
 diceAmountType = raw_input("Enter dice amount and type as XdX: ")
 print (diceAmountType)
-diceModifier = raw_input("Enter dice modifier: ")
-print (diceModifier)
 print '\n'
 diceAmount = diceAmountType.split("d", 1)[0]
 diceType = int(diceAmountType.split("d", 2)[1])
 total = 0
 rollSum = 0
-modifierSum = 0
 natOnes = 0
 natTwenties = 0
 
 for x in range(1, (int(diceAmount) + 1)):
     roll = randrange(1, (diceType) + 1)
-    totalRoll = (roll + int(diceModifier))
+    totalRoll = roll 
     if diceType == 20:
         if roll == 1:
                 natOnes += 1
-                print("Roll " + str(x) + ": " + "Natural 1 :(" + " (" + str(roll) + " + " + str(diceModifier) + ")")
+                print "Roll " + str(x) + ": " + "Natural 1 " + str(roll)
         elif roll == 20:
                 natTwenties += 1
-                print("Roll " + str(x) + ": " + "Natural 20! :)" + " (" + str(roll) + " + " + str(diceModifier) + ")")
+                print "Roll " + str(x) + ": " + "Natural 20! " + str(roll)
         else:
-                print("Roll " + str(x) + ": " + str(totalRoll) + " (" + str(roll) + " + " + str(diceModifier) + ")")
+                print "Roll " + str(x) + ": " + str(totalRoll) + str(roll)
     else:
-        print("Roll " + str(x) + ": " + str(totalRoll) + " (" + str(roll) + " + " + str(diceModifier) + ")")
+        print "Roll " + str(x) + ": " + str(totalRoll) + str(roll)
     rollSum = rollSum + roll
-    modifierSum = modifierSum + int(diceModifier)
     total = total + totalRoll
 print '\n'
-print ("Total: "+ str(total) + " (" + str(rollSum) + " + " + str(modifierSum) + ")")
+print "Total: "+ str(total) + str(rollSum)
 if diceType == 20:
         print ("Natural 1's: " + str(natOnes))
         print ("Natural 20's: " + str(natTwenties))
--- dungeon.py
+++ dungeon.py
@@ -6,7 +6,7 @@ import re
 
 print """
 You yawn widely, and scratch Your head.
- 'What happend yesterday? I can't remember anything...
+ 'Where am I? What happend last night? I can't remember anything...
   Last moment, last thing that I remember was this...person...thing...
   Who or what was that?'
 """
@@ -19,13 +19,32 @@ def char_race():
     while True:
         race=raw_input("Please select Your race (Orc/Elf/Human/Dwarf): ")
         if race.isalpha() and race in race_list:
-            #print "You are " + race + " !"
+            if race=='Orc':
+                print """
+You see an old Orc Shaman, with only one eye. 
+He's old, very old, even for orcish standards.
+'You come, me got gift for you' - screamed the old Orc.
+A sudden an unknown curiosity forced You to move towards him.
+'You take, no ask'
+The same curiosity told You to take the gift - strange looking key,
+with an orcish little skull.
+'You know where, You sleep now' yelled the Shaman and when You started
+to realize whats happening, darkness came and You fell asleep.
+You woke up, near a strange looking mansion, with a key in Your pocket.
+'What the hell is going on?'
+Driven by the same feeling You had earlier, You take Your axe and start
+walking towards the building.
+"""
+	    elif race=='Elf':
+                print "You are an elf"
             return race
             break
         else:
             print "You must choose a valid race!"
             continue
 
+
+char_race()
 
 # Function to provide character  name
 def char_name():
@@ -39,44 +58,34 @@ def char_name():
             print "You must type letters! Try again..."
             continue
 
+char_name()
 
-# Function to provide character age
-def char_age():
+# Function to choose first room
+def first_room():
     while True:
-        age=raw_input("How old are You? :")
-        if age.isdigit():
-            #print "You are " + age + " !"
-            return str(age)
-            break
-        else:
-            print "You must type digits! Try again..."
-            continue
-
-print "Greetings " + "," + char_race() + " "  +  char_name() + " ! " + "You are " + char_age() + " years old" +  "."
-
-# Function to choose first chamber
-def first_chamber():
-    while True:
-        first_chamber={
+        first_room={
         'Left': 'Military quarters',
         'Center': 'Cellar',
         'Right': 'Servants quarters'
 }
         direction=raw_input("Where would You like to go? (Left/Right/Center): ")
-        for key in first_chamber.items():
-            if direction in first_chamber:
+        for key in first_room.items():
+            if direction in first_room:
                 if direction=='Left':
-                    print "You enter " + first_chamber[direction] + "." + " Watch out for undead troops!!!"
+                    print "You enter " + first_room[direction] + "." + " Watch out for undead troops!!!"
                 elif direction=='Right':
-                    print "You enter " + first_chamber[direction] + "." + " Something or someone can eat You here...Watch Your back..."
+                    print "You enter " + first_room[direction] + "." + " Something or someone can eat You here...Watch Your back..."
                 elif direction=='Center':
-                    print "You enter " + first_chamber[direction] + "." + " Whats crawling below?"
-                #print first_chamber[direction]
-                return first_chamber[direction]
+                    print "You enter " + first_room[direction] + "." + " Whats crawling below?"
+                #print first_room[direction]
+                return first_room[direction]
                 break
             else:
                 print "Wrong direction, choose again..."
                 continue
 
-first_chamber()
+first_room()
 
+
+print "You encounter a zombie"
+import dice_roll
