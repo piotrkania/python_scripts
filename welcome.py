1fa1b2b2adee1d580b7c5cdfb1448b6122fb34f1
piotrkania <36471063+piotrkania@users.noreply.github.com>
2018-04-13 14:34:40 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -2,27 +2,13 @@
 
 print """\
 
-         WW         WW EEEEEEE LL       CCCCC   OOOOO  MM    MM EEEEEEE
-          WW       WW  EE      LL      CC    C OO   OO MMM  MMM EE
-          WW   W   WW  EEEEE   LL      CC      OO   OO MM MM MM EEEEE
-           WW WWW WW   EE      LL      CC    C OO   OO MM    MM EE
-            WW   WW    EEEEEEE LLLLLLL  CCCCC   OOOO0  MM    MM EEEEEEE
+ ______            _        _______  _______  _______  _          _______           _
+(  __  \ |\     /|( (    /|(  ____ \(  ____ \(  ___  )( (    /|  (  ____ )|\     /|( (    /|
+| (  \  )| )   ( ||  \  ( || (    \/| (    \/| (   ) ||  \  ( |  | (    )|| )   ( ||  \  ( |
+| |   ) || |   | ||   \ | || |      | (__    | |   | ||   \ | |  | (____)|| |   | ||   \ | |
+| |   | || |   | || (\ \) || | ____ |  __)   | |   | || (\ \) |  |     __)| |   | || (\ \) |
+| |   ) || |   | || | \   || | \_  )| (      | |   | || | \   |  | (\ (   | |   | || | \   |
+| (__/  )| (___) || )  \  || (___) || (____/\| (___) || )  \  |  | ) \ \__| (___) || )  \  |
+(______/ (_______)|/    )_)(_______)(_______/(_______)|/    )_)  |/   \__/(_______)|/    )_)
 
-                   TTTTTTT  OOOOO   TTTTTTT HH   HH EEEEEEE
-                     TTT   OO   OO    TTT   HH   HH EE
-                     TTT   OO   OO    TTT   HHHHHHH EEEEE
-                     TTT   OO   OO    TTT   HH   HH EE
-                     TTT    OOOO0     TTT   HH   HH EEEEEEE
-
-            DDDDD   UU   UU NN   NN   GGGG  EEEEEEE  OOOOO  NN   NN
-            DD  DD  UU   UU NNN  NN  GG  GG EE      OO   OO NNN  NN
-            DD   DD UU   UU NN N NN GG      EEEEE   OO   OO NN N NN
-            DD   DD UU   UU NN  NNN GG   GG EE      OO   OO NN  NNN
-            DDDDDD   UUUUU  NN   NN  GGGGGG EEEEEEE  OOOO0  NN   NN
-
-                            RRRRRR  UU   UU NN   NN
-                            RR   RR UU   UU NNN  NN
-                            RRRRRR  UU   UU NN N NN
-                            RR  RR  UU   UU NN  NNN
-                            RR   RR  UUUUU  NN   NN
-"""
+""" 
